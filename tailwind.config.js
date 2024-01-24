bd81cf801f48b423957d51347e44603a8fa72cd3
sscoderati <chungup5495@gmail.com>
2024-01-24 20:38:24 +0900

    Feat: shadcn-ui 관련 셋업

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+	darkMode: ['class'],
+	content: [
+		'./src/**/*.{astro,html,js,md,mdx,ts}',
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}'
+	],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			colors: {
+				white: '#f8f9fa'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
+			},
+			fontFamily: {
+				body: ['Manrope', ...defaultTheme.fontFamily.sans]
+			},
+			gridTemplateColumns: {
+				list: 'repeat(auto-fill, minmax(400px, max-content))'
+			}
+		}
+	},
+	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
+}
