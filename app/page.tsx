8f111c4342411849297bfe8fc5c01bd13e82c0ca
Changgi Hong <chungup5495@gmail.com>
2026-04-19 02:28:31 +0900

    feat: migrate logs content to wiki and remove logs routes (#54)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import HomeClient from '@/components/widget/home-client'
 import { getSortedBlogListItems } from '@/lib/blog'
-import { getLogSeries } from '@/lib/log-posts'
 
-export default async function HomePage() {
+export default function HomePage() {
   const blogPosts = getSortedBlogListItems()
-  const logCategories = await getLogSeries()
 
-  return <HomeClient blogPosts={blogPosts} logCategories={logCategories} />
+  return <HomeClient blogPosts={blogPosts} />
 }
